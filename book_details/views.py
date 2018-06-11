d387e1087115cf1ead4d84eeccce9d0a918da587
Pitutu <piotrekgrochalski@gmail.com>
2018-06-11 19:03:45 +0200

    interfejs jaki byl pokazywany

--- book_details/views.py
+++ book_details/views.py
@@ -17,4 +17,6 @@ def book_details(request, book_id, reported=False):
 
 def book_details_short(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
-    return render(request, 'book_details/book_short.html', {'book': book})
+    images = Images.objects
+    images = images.filter(bookId=book)
+    return render(request, 'book_details/book_short.html', {'book': book, 'images': images})
